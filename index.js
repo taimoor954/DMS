4d1455d1aa7b5ed513d257c00446d6adbb3745d2
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-12 19:33:46 +0500

    Routes boilerlpate for dr created

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+const {doctorRouter} = require('./Routes/doctorRoutes.js')
+
 const app = express();
+//CONFIG SERCRETS FROM config.env FILE
 dotenv.config({
   path: `${__dirname}/config.env`,
 });
-const DB = process.env.DATABASE_LOCAL;
 
+//MONGO DB CONNECTION
+const DB = process.env.DATABASE_LOCAL;
 const getMongoConnection = async () => {
   try {
     await mongoose.connect(DB, {
@@ -22,8 +28,11 @@ const getMongoConnection = async () => {
 };
 getMongoConnection();
 
-const port = process.env.PORT;
+app.use('/api/v1/doctor', doctorRouter)
 
+
+//SERVER LISTENING  
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Listening at port.... ${port}`);
 });
