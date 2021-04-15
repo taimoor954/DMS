8db240778fd802dee8623c6cb9f36cccc5332dd2
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-15 15:40:53 +0500

    done with protectRouteMiddleware

--- Controller/doctorAuthentication.js
+++ Controller/doctorAuthentication.js
@@ -1,9 +1,15 @@
 const { response } = require('express');
 const validator = require('validator');
+const dotenv = require('dotenv');
 const { AppError } = require('../utils/Error');
 const { Doctor } = require('../Model/doctorModel');
 const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
+const path = require('path');
+const configDIR = path.join(__dirname, '../config.env');
+
+dotenv.config({ path: configDIR });
+console.log(process.env.SECRET_TOKEN_STRING);
 
 const tokenGenerator = (id) => {
   return jwt.sign(
@@ -38,12 +44,43 @@ exports.login = catchAsync(async (request, response, next) => {
       new AppError('Something wrong with either email or password', 400)
     );
   }
-
+  // console.log(request.headers);
   //if correct then send jwt in response
   return response.status(200).json({
     success: 'Succesfully logged in!!',
     doctor,
-    token:tokenGenerator(doctor._id)
-    
+    token: tokenGenerator(doctor._id),
   });
 });
+
+exports.protectedRouteMiddleware = catchAsync(
+  async (request, response, next) => {
+    let token;
+    //GET TOKEN FROM HEADER (FOR DEVELOPMENT PURPOSE JUST)
+    if (
+      request.headers.authorization &&
+      request.headers.authorization.startsWith('Bearer')
+    ) {
+      token = request.headers.authorization.split(' ')[1]; //split and get seocnd elemet
+    }
+    if (!token) {
+      return next(new AppError('You are not logged in .Please log in', 401));
+    }
+
+    //CHECK IF TOKEN IS FINE
+    const decodedToken = await jwt.verify(
+      token,
+      process.env.SECRET_TOKEN_STRING
+    ); // the above one ant this one both return promise
+    //CHECK IF USER CONTAINING THIS TOKEN STILL EXIST
+    const freshUser = await Doctor.findById(decodedToken.id);
+    if (!freshUser)
+      return next(
+        new AppError('The user belonging to this token is no longer exist', 401)
+      );
+    //GRANT ACCESS IF IT ACCESS
+    request.doctor = freshUser;
+
+    next();
+  }
+);
--- Routes/doctorRoutes.js
+++ Routes/doctorRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { login } = require('../Controller/doctorAuthentication');
+const { login, protectedRouteMiddleware } = require('../Controller/doctorAuthentication');
 const {
   getAllDoctors,
   createDoctor,
@@ -11,6 +11,7 @@ const router = express.Router();
 
 router.route('/').get(getAllDoctors).post(createDoctor);
 router.route('/login-doctor').post(login);
+router.route('/protect').get(protectedRouteMiddleware);
 
 router
   .route('/:doctorId')
