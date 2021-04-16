b0b0df9008beb8d264cc172cd02e8f60a2deba64
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-16 15:59:14 +0500

    restric user added

--- middleware/protectedMiddleware.js
+++ middleware/protectedMiddleware.js
@@ -1,4 +1,4 @@
-const catchAsync = require("../utils/catchAsync");
+const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const { AppError } = require('../utils/Error');
@@ -9,32 +9,40 @@ dotenv.config({ path: configDIR });
 exports.protectedRouteMiddleware = (Model) =>
   catchAsync(async (request, response, next) => {
     let token;
-    //GET TOKEN FROM HEADER (FOR DEVELOPMENT PURPOSE JUST)
+
     if (
       request.headers.authorization &&
       request.headers.authorization.startsWith('Bearer')
     ) {
-      token = request.headers.authorization.split(' ')[1]; //split and get seocnd elemet
+      token = request.headers.authorization.split(' ')[1];
     }
     if (!token) {
       return next(new AppError('You are not logged in .Please log in', 401));
     }
 
-    //CHECK IF TOKEN IS FINE
     const decodedToken = await jwt.verify(
       token,
       process.env.SECRET_TOKEN_STRING
-    ); // the above one ant this one both return promise
-    //CHECK IF USER CONTAINING THIS TOKEN STILL EXIST
+    );
+
     const freshUser = await Model.findById(decodedToken.id);
     if (!freshUser)
       return next(
         new AppError('The user belonging to this token is no longer exist', 401)
       );
-    //GRANT ACCESS IF IT ACCESS
-    if(freshUser.role == "doctor")   request.doctor = freshUser;
-    else if(freshUser.role == 'patient') request.patient = freshUser
-  
+
+    request.user = freshUser;
 
     next();
   });
+
+exports.restrictUser = (...roles) => {
+  return (request, response, next) => {
+    if (!roles.includes(request.user.role)) {
+      return next(
+        new AppError('You do not have permisson to perform this action', 403)
+      );
+    }
+    next();
+  };
+};
