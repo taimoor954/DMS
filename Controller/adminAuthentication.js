028618e7682b7afb1e442751d158aaf6df314995
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-20 16:33:51 +0500

    admin auth added

---
+++ Controller/adminAuthentication.js
@@ -0,0 +1,40 @@
+const { response } = require('express');
+const validator = require('validator');
+const dotenv = require('dotenv');
+const { AppError } = require('../utils/Error');
+const jwt = require('jsonwebtoken');
+const catchAsync = require('../utils/catchAsync');
+const path = require('path');
+const {  login, protectRouteMiddleware } = require('../middleware/middleware');
+const { Admin } = require('../Model/adminModel');
+const configDIR = path.join(__dirname, '../config.env');
+
+dotenv.config({ path: configDIR });
+
+exports.login = login(Admin);
+
+exports.protectAdminRoutes = catchAsync(async (request, response, next) => {
+  let token;
+  if (
+    request.headers.authorization &&
+    request.headers.authorization.startsWith('Bearer')
+  ) {
+    token = request.headers.authorization.split(' ')[1];
+  }else if (request.cookies.jwt) {
+    token = request.cookies.jwt;
+  }
+  
+  if (!token) {
+    return next(new AppError('You are not logged in .Please log in', 401));
+  }
+
+  const decodedToken = await jwt.verify(token, process.env.SECRET_TOKEN_STRING);
+  const freshUser = await Admin.findById(decodedToken.id);
+  if (!freshUser)
+    return next(
+      new AppError('The user belonging to this token is no longer exist', 401)
+    );
+
+  request.user = freshUser;
+  next();
+});
