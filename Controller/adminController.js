46b06256e71afbf79b5918c909752e75929e59b7
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-20 16:07:57 +0500

    admin Model added and admin authentication addded

---
+++ Controller/adminController.js
@@ -0,0 +1,35 @@
+// const { Appointment } = require('../Model/appointmentModel');
+const { Admin } = require('../Model/adminModel');
+const catchAsync = require('../utils/catchAsync');
+const {
+  getAllFactory,
+  createFactory,
+  getOneFactoryById,
+  updateFactory,
+  deleteFactory,
+} = require('./handlerFactory');
+
+exports.getAllAdmins = getAllFactory(Admin);
+
+//TO GET SPECFIC DR BY PROVIDING HIS/HER ID
+exports.getAdminById = getOneFactoryById(Admin);
+
+//CREATE A NEW Admin
+//SHOULD ONLY BE PERMITTED TO ADMIN I GUESS
+//WORK AS SIGNUP
+exports.createAdmin = createFactory(Admin);
+//SHOULD ONLY BE PERMITTED TO ADMIN & Admin HIMSELF I GUESS
+
+exports.updateAdmin = updateFactory(Admin);
+
+//SHOULD ONLY BE PERMITTED TO ADMIN & Admin HIMSELF I GUESS
+
+exports.deleteAdmin = deleteFactory(Admin);
+
+
+// exports.getMyPatients = catchAsync(async(request, response, next) => {
+// const appointments = await Appointment.find({doctor : request.user.id})
+// response.status(200).json({
+//   patients : appointments
+// })
+// })
---
+++ Model/adminModel.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please insert a valid email'],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'doctor', 'admin'],
+    default: 'admin',
+  },
+  
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: 4,
+    select: false, //will not be shown up when data will be fetched from database
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Password Confirmation is required'],
+    validate: {
+      //this only work on save or create not on find one and update
+      validator: function (passConfirm) {
+        return passConfirm === this.password; //if pass confirm then true else false
+      },
+      message: 'Sorry passwords are diffrent', //err message
+    },
+  },
+});
+
+
+//hash pass before saving doc
+adminSchema.pre('save', async function (next) {
+  //if the pass is modified only then encrypt dontt encrypt again and again
+  //  when email or other fields are modifier
+  if (!this.isModified('password')) return next();
+
+  //hash pass with cost of 12 //jitna ziada hash cost utna ziada time taken and utna ziada enrypted
+  this.password = await bcrypt.hash(this.password, 12);
+  //delete pass comfirm field
+  this.passwordConfirm = undefined;
+});
+adminSchema.methods.matchPassword = async function (
+  requestBodyPassword,
+  storedPassword
+) {
+  return await bcrypt.compare(storedPassword, requestBodyPassword); //rettuns bool
+};
+
+const Admin = mongoose.model('Admin', adminSchema);
+exports.Admin = Admin;
