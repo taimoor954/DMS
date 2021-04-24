fee2ee4f8fa83e5275613395037e3a76674c2ef1
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-24 16:54:27 +0500

    changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const compression = require('compression')
 const helmet = require('helmet');
 const app = express();
 var corsOptions = {
-  origin: 'http://127.0.0.1:5500/',
+  origin: 'localhost:5500',
   credentials: true,
 };
 app.use(cors(corsOptions))
