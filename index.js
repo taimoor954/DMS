e3beeebf8e7da5aa01944bde23e2ce28128f85a3
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-27 00:16:01 +0500

    changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const helmet = require('helmet');
 const app = express();
 var corsOptions = {
   origin: 'http://127.0.0.1:5500',
-  credentials: "same-origin",
+  credentials: true,
 };
 app.use(cors(corsOptions))
 app.use(cookie_parser());
@@ -40,26 +40,26 @@ app.use(function (request, response, next) {
   next();
 });
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,content-type,set-cookie'
-  );
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+//   );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin,X-Requested-With,content-type,set-cookie'
+//   );
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 
 if (process.env.NODE_ENV == 'development') {
