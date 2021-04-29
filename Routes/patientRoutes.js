3e7c5b801a82cd74b1f5b030a8415c375d42e290
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-30 03:00:03 +0500

    restrict user added

--- Routes/patientRoutes.js
+++ Routes/patientRoutes.js
@@ -10,17 +10,17 @@ const {
   deletePatient,
   getMyDoctors,
 } = require('../Controller/patientController');
-const { logout } = require('../middleware/middleware');
+const { logout, restrictUser } = require('../middleware/middleware');
 
 router.route('/').get(protectPatientRoutes,getAllPatients) //api will be use by just admin side
 .post(createPatient); //api will be use by patient side just
 
 router.route('/login-patient').post(login); //api will be use by patient side just
 
-router.route('/getAllDoctors').get(protectPatientRoutes,getAllDoctors)// api will be use by both client and doctor side
+router.route('/getAllDoctors').get(protectPatientRoutes,restrictUser('patient'),getAllDoctors)// api will be use by both client and doctor side
 
 router.route('/getmydoctors')
-.get(protectPatientRoutes,getMyDoctors) //api will be used by logged in patient
+.get(protectPatientRoutes,restrictUser('patient'),getMyDoctors) //api will be used by logged in patient
 
 router
   .route('/:Id')
