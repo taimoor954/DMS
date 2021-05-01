b627af852ef9d7cc9e7763acf13fd369e093c859
taimoor954 <taimoormuhammad954@gmail.com>
2021-05-01 20:45:19 +0500

    changes in logout

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -93,6 +93,8 @@ exports.logout = (request, response) => {
   response.cookie('jwt', 'loggedOut', {
     expires: new Date(Date.now() + 10 * 1000), //tampered token will be given for 10 seconds and then the user will be logged out
     httpOnly: false,
+    sameSite: 'None',
+
   });
   response.status(200).json({
     status: 'succesfully loggedout',
